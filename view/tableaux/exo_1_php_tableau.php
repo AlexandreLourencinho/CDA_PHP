9d68db25f2a67da2d74a90f17fcb1ce3bcec30c1
Alexandre Lourencinho <alexandre.lourencinho@gmail.com>
2021-05-03 17:56:10 +0200

    correction bonnes pratiques tableaux

--- view/tableaux/exo_1_php_tableau.php
+++ view/tableaux/exo_1_php_tableau.php
@@ -6,38 +6,50 @@ include "controller/tableaux/exo_1_php_tableau_controller.php";
     <h1></h1>
     <a href="https://ncode.amorce.org/ressources/Pool/CDA/WEB_PHP_frc/PHP_04_Tableaux.html"
        title="Le lien vers l'énoncé">Lien vers l'énoncé de l'exercice</a>
-<div class="d-flex flex-row justify-content-start">
-<div class="table-responsive col-2">
-    <table class=" table table-bordered table-dark col-1">
-        <thead>
-        <tr>
-
-<?php foreach ($tableaumois as $mois=>$nombreJours) { ?>
-            <th>
-                <?= $mois ?>
-            </th>
-    <td> <?= $nombreJours ?></td>
-        </tr>
-        <?php } ?>
-
-        </thead>
-    </table>
-</div>
-    <div class="table-responsive">
-        <table class=" table table-bordered table-dark col-1">
-            <thead>
-<?php
-arsort($tableaumois);
-foreach ($tableaumois as $mois=>$nombreJours) { ?>
-            <tr>
-                <th>
-                        <?= $mois ?>
-                    </th> <td> <?= $nombreJours ?> </td></tr>
+    <div class="d-flex flex-row justify-content-start">
+        <div class="table-responsive col-2">
+            <table class=" table table-bordered table-dark col-1">
+                <thead>
+                <tr>
+                    <th>
+                        mois
+                    </th>
+                    <th>
+                        nombre de jours
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($tableaumois as $mois => $nombreJours) { ?>
+                    <tr>
+                        <td> <?= $mois ?></td>
+                        <td> <?= $nombreJours ?></td>
+                    </tr>
                 <?php } ?>
-            </thead>
-        </table>
+                </tbody>
+            </table>
+        </div>
+        <div class="table-responsive">
+            <table class=" table table-bordered table-dark col-1">
+                <thead>
+                <tr>
+                    <th>Mois</th>
+                    <th>Nombre de jours</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                arsort($tableaumois);
+                foreach ($tableaumois as $mois => $nombreJours) { ?>
+                    <tr>
+                        <td><?= $mois ?></td>
+                        <td> <?= $nombreJours ?> </td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 <?php
 include "view/footer.php";
 ?>
--- view/tableaux/tableaux_exo_2.php
+++ view/tableaux/tableaux_exo_2.php
@@ -4,43 +4,56 @@ include "view/header.php";
 include "controller/tableaux/tableaux_exo_2_controller.php";
 ?>
 
-<div class="d-flex flex-row justify-content-start">
-    <span class="h5"> premier tableau</span>
-    <div class="table-responsive col-2 p-0">
-        <table class=" table table-bordered table-dark col-3">
+    <div class="d-flex flex-row justify-content-start">
+        <span class="h5"> premier tableau</span>
+        <div class="table-responsive col-2 p-0">
+            <table class=" table table-bordered table-dark col-3">
+                <thead>
+                <tr>
+                    <th>Ville</th>
+                    <th>Pays</th>
+                </tr>
+                </thead>
+                <tbody>
                 <?php
                 ksort($capitales);
                 foreach ($capitales as $ville => $pays) {
                     ?>
-                <tr>
-                    <th><?= $ville ?></th>
-                    <td><?= $pays ?></td>
-                </tr>
+                    <tr>
+                        <td><?= $ville ?></td>
+                        <td><?= $pays ?></td>
+                    </tr>
                 <?php } ?>
-        </table>
-    </div>
-    <span class="h5">Deuxième tableau</span>
-    <div class="table-responsive col-3 p-0">
-    <table class=" table table-bordered table-dark col-3">
-        <thead>
-
-            <?php
-            asort($capitales);
-            foreach ($capitales as $ville => $pays) {
-                ?>
+                </tbody>
+            </table>
+        </div>
+        <span class="h5">Deuxième tableau</span>
+        <div class="table-responsive col-3 p-0">
+            <table class=" table table-bordered table-dark col-3">
+                <thead>
                 <tr>
-                <th><?= $pays ?></th>
-                <td><?= $ville ?></td>
-        </tr>
-            <?php } ?>
-        </thead>
-    </table>
+                    <th>Pays</th>
+                    <th>Ville</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                asort($capitales);
+                foreach ($capitales as $ville => $pays) {
+                    ?>
+                    <tr>
+                        <td><?= $pays ?></td>
+                        <td><?= $ville ?></td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        </div>
+        <?php
+        $nombre = count($capitales)
+        ?>
+        <p>Il y a <?= $nombre ?> villes.</p>
     </div>
-    <?php
-    $nombre=count($capitales)
-    ?>
-    <p>Il y a <?= $nombre ?> villes.</p>
-</div>
 
 <?php
 include "view/footer.php";
