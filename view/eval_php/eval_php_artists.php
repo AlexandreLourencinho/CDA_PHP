591cb8ea08d6edad6444e249a29fae0c5568aeab
Alexandre <alexandre.lourencinho@gmail.com>
2021-05-12 01:19:23 +0200

    recherche explications php , a verifier auprès formateur2

--- view/eval_php/eval_php_artists.php
+++ view/eval_php/eval_php_artists.php
@@ -6,15 +6,16 @@ include "./model/eval_php_model/requete/eval_php_requete.php";
 
 
 <?php foreach ($tableauArtistes as $nomArtistes){
-    ?>
+   var_dump($nomArtistes); ?>
 <p><?= $nomArtistes->artist_name ?></p>
+<!--    forme d'objet, donc pas $nomArtistes[artist_name] mais $nomArtistes->artist_name-->
 <?php
 }
     ?>
 
 <?php
 foreach ($tableaudisques as $disques) {
-//    var_dump($disques);
+    var_dump($disques);
     ?>
     <p><span><a target="_blank" href="/view/eval_php/eval_php_details_disques.php?disc_id=<?= $disques->disc_id; ?>"><?= $disques->disc_id ?></a></span>
     <span> <?= $disques->disc_genre ?></span>
